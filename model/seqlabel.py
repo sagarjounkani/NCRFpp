e2a0d54a07ab2f83d4576d41926c5dbc802736f9
Sagar Jounkani <sagar@sagarsmac.local>
2020-05-12 18:41:01 +0530

    some updates

--- model/seqlabel.py
+++ model/seqlabel.py
@@ -12,6 +12,7 @@ import torch.nn.functional as F
 from .wordsequence import WordSequence
 from .crf import CRF
 
+
 class SeqLabel(nn.Module):
     def __init__(self, data):
         super(SeqLabel, self).__init__()
@@ -32,9 +33,10 @@ class SeqLabel(nn.Module):
         if self.use_crf:
             self.crf = CRF(label_size, self.gpu)
 
-
-    def calculate_loss(self, word_inputs, feature_inputs, word_seq_lengths, char_inputs, char_seq_lengths, char_seq_recover, batch_label, mask):
-        outs = self.word_hidden(word_inputs,feature_inputs, word_seq_lengths, char_inputs, char_seq_lengths, char_seq_recover)
+    def calculate_loss(self, word_inputs, feature_inputs, word_seq_lengths, char_inputs, char_seq_lengths,
+                       char_seq_recover, batch_label, mask):
+        outs = self.word_hidden(word_inputs, feature_inputs, word_seq_lengths, char_inputs, char_seq_lengths,
+                                char_seq_recover)
         batch_size = word_inputs.size(0)
         seq_len = word_inputs.size(1)
         if self.use_crf:
@@ -45,39 +47,39 @@ class SeqLabel(nn.Module):
             outs = outs.view(batch_size * seq_len, -1)
             score = F.log_softmax(outs, 1)
             total_loss = loss_function(score, batch_label.view(batch_size * seq_len))
-            _, tag_seq  = torch.max(score, 1)
+            _, tag_seq = torch.max(score, 1)
             tag_seq = tag_seq.view(batch_size, seq_len)
         if self.average_batch:
             total_loss = total_loss / batch_size
         return total_loss, tag_seq
 
-
-    def forward(self, word_inputs, feature_inputs, word_seq_lengths, char_inputs, char_seq_lengths, char_seq_recover, mask):
-        outs = self.word_hidden(word_inputs,feature_inputs, word_seq_lengths, char_inputs, char_seq_lengths, char_seq_recover)
+    def forward(self, word_inputs, feature_inputs, word_seq_lengths, char_inputs, char_seq_lengths, char_seq_recover,
+                mask):
+        outs = self.word_hidden(word_inputs, feature_inputs, word_seq_lengths, char_inputs, char_seq_lengths,
+                                char_seq_recover)
         batch_size = word_inputs.size(0)
         seq_len = word_inputs.size(1)
         if self.use_crf:
             scores, tag_seq = self.crf._viterbi_decode(outs, mask)
         else:
             outs = outs.view(batch_size * seq_len, -1)
-            _, tag_seq  = torch.max(outs, 1)
+            _, tag_seq = torch.max(outs, 1)
             tag_seq = tag_seq.view(batch_size, seq_len)
             ## filter padded position with zero
             tag_seq = mask.long() * tag_seq
         return tag_seq
 
-
     # def get_lstm_features(self, word_inputs, word_seq_lengths, char_inputs, char_seq_lengths, char_seq_recover):
     #     return self.word_hidden(word_inputs, word_seq_lengths, char_inputs, char_seq_lengths, char_seq_recover)
 
-
-    def decode_nbest(self, word_inputs, feature_inputs, word_seq_lengths, char_inputs, char_seq_lengths, char_seq_recover, mask, nbest):
+    def decode_nbest(self, word_inputs, feature_inputs, word_seq_lengths, char_inputs, char_seq_lengths,
+                     char_seq_recover, mask, nbest):
         if not self.use_crf:
             print("Nbest output is currently supported only for CRF! Exit...")
             exit(0)
-        outs = self.word_hidden(word_inputs,feature_inputs, word_seq_lengths, char_inputs, char_seq_lengths, char_seq_recover)
+        outs = self.word_hidden(word_inputs, feature_inputs, word_seq_lengths, char_inputs, char_seq_lengths,
+                                char_seq_recover)
         batch_size = word_inputs.size(0)
         seq_len = word_inputs.size(1)
         scores, tag_seq = self.crf._viterbi_decode_nbest(outs, mask, nbest)
         return scores, tag_seq
-
--- utils/functions.py
+++ utils/functions.py
@@ -8,6 +8,7 @@ from __future__ import absolute_import
 import sys
 import numpy as np
 
+
 def normalize_word(word):
     new_word = ""
     for char in word:
@@ -18,9 +19,11 @@ def normalize_word(word):
     return new_word
 
 
-def read_instance(input_file, word_alphabet, char_alphabet, feature_alphabets, label_alphabet, number_normalized, max_sent_length, sentence_classification=False, split_token='\t', char_padding_size=-1, char_padding_symbol = '</pad>'):
+def read_instance(input_file, word_alphabet, char_alphabet, feature_alphabets, label_alphabet, number_normalized,
+                  max_sent_length, sentence_classification=False, split_token='\t', char_padding_size=-1,
+                  char_padding_symbol='</pad>'):
     feature_num = len(feature_alphabets)
-    in_lines = open(input_file,'r', encoding="utf8").readlines()
+    in_lines = open(input_file, 'r', encoding="utf8").readlines()
     instence_texts = []
     instence_Ids = []
     words = []
@@ -54,8 +57,8 @@ def read_instance(input_file, word_alphabet, char_alphabet, feature_alphabets, l
                     if char_padding_size > 0:
                         char_number = len(char_list)
                         if char_number < char_padding_size:
-                            char_list = char_list + [char_padding_symbol]*(char_padding_size-char_number)
-                        assert(len(char_list) == char_padding_size)
+                            char_list = char_list + [char_padding_symbol] * (char_padding_size - char_number)
+                        assert (len(char_list) == char_padding_size)
                     for char in char_list:
                         char_Id.append(char_alphabet.get_index(char))
                     chars.append(char_list)
@@ -67,13 +70,13 @@ def read_instance(input_file, word_alphabet, char_alphabet, feature_alphabets, l
                 feat_list = []
                 feat_Id = []
                 for idx in range(feature_num):
-                    feat_idx = pairs[idx+1].split(']',1)[-1]
+                    feat_idx = pairs[idx + 1].split(']', 1)[-1]
                     feat_list.append(feat_idx)
                     feat_Id.append(feature_alphabets[idx].get_index(feat_idx))
                 ## combine together and return, notice the feature/label as different format with sequence labeling task
                 if (len(words) > 0) and ((max_sent_length < 0) or (len(words) < max_sent_length)):
                     instence_texts.append([words, feat_list, chars, label])
-                    instence_Ids.append([word_Ids, feat_Id, char_Ids,label_Id])
+                    instence_Ids.append([word_Ids, feat_Id, char_Ids, label_Id])
                 words = []
                 features = []
                 chars = []
@@ -81,9 +84,9 @@ def read_instance(input_file, word_alphabet, char_alphabet, feature_alphabets, l
                 word_Ids = []
                 feature_Ids = []
                 label_Ids = []
-        if (len(words) > 0) and ((max_sent_length < 0) or (len(words) < max_sent_length)) :
+        if (len(words) > 0) and ((max_sent_length < 0) or (len(words) < max_sent_length)):
             instence_texts.append([words, feat_list, chars, label])
-            instence_Ids.append([word_Ids, feat_Id, char_Ids,label_Id])
+            instence_Ids.append([word_Ids, feat_Id, char_Ids, label_Id])
             words = []
             features = []
             chars = []
@@ -93,7 +96,7 @@ def read_instance(input_file, word_alphabet, char_alphabet, feature_alphabets, l
             label_Ids = []
 
     else:
-    ### for sequence labeling data format i.e. CoNLL 2003
+        ### for sequence labeling data format i.e. CoNLL 2003
         for line in in_lines:
             if len(line) > 2:
                 pairs = line.strip().split()
@@ -111,7 +114,7 @@ def read_instance(input_file, word_alphabet, char_alphabet, feature_alphabets, l
                 feat_list = []
                 feat_Id = []
                 for idx in range(feature_num):
-                    feat_idx = pairs[idx+1].split(']',1)[-1]
+                    feat_idx = pairs[idx + 1].split(']', 1)[-1]
                     feat_list.append(feat_idx)
                     feat_Id.append(feature_alphabets[idx].get_index(feat_idx))
                 features.append(feat_list)
@@ -124,8 +127,8 @@ def read_instance(input_file, word_alphabet, char_alphabet, feature_alphabets, l
                 if char_padding_size > 0:
                     char_number = len(char_list)
                     if char_number < char_padding_size:
-                        char_list = char_list + [char_padding_symbol]*(char_padding_size-char_number)
-                    assert(len(char_list) == char_padding_size)
+                        char_list = char_list + [char_padding_symbol] * (char_padding_size - char_number)
+                    assert (len(char_list) == char_padding_size)
                 else:
                     ### not padding
                     pass
@@ -134,9 +137,9 @@ def read_instance(input_file, word_alphabet, char_alphabet, feature_alphabets, l
                 chars.append(char_list)
                 char_Ids.append(char_Id)
             else:
-                if (len(words) > 0) and ((max_sent_length < 0) or (len(words) < max_sent_length)) :
+                if (len(words) > 0) and ((max_sent_length < 0) or (len(words) < max_sent_length)):
                     instence_texts.append([words, features, chars, labels])
-                    instence_Ids.append([word_Ids, feature_Ids, char_Ids,label_Ids])
+                    instence_Ids.append([word_Ids, feature_Ids, char_Ids, label_Ids])
                 words = []
                 features = []
                 chars = []
@@ -145,9 +148,9 @@ def read_instance(input_file, word_alphabet, char_alphabet, feature_alphabets, l
                 feature_Ids = []
                 char_Ids = []
                 label_Ids = []
-        if (len(words) > 0) and ((max_sent_length < 0) or (len(words) < max_sent_length)) :
+        if (len(words) > 0) and ((max_sent_length < 0) or (len(words) < max_sent_length)):
             instence_texts.append([words, features, chars, labels])
-            instence_Ids.append([word_Ids, feature_Ids, char_Ids,label_Ids])
+            instence_Ids.append([word_Ids, feature_Ids, char_Ids, label_Ids])
             words = []
             features = []
             chars = []
@@ -172,26 +175,29 @@ def build_pretrain_embedding(embedding_path, word_alphabet, embedd_dim=100, norm
     for word, index in word_alphabet.iteritems():
         if word in embedd_dict:
             if norm:
-                pretrain_emb[index,:] = norm2one(embedd_dict[word])
+                pretrain_emb[index, :] = norm2one(embedd_dict[word])
             else:
-                pretrain_emb[index,:] = embedd_dict[word]
+                pretrain_emb[index, :] = embedd_dict[word]
             perfect_match += 1
         elif word.lower() in embedd_dict:
             if norm:
-                pretrain_emb[index,:] = norm2one(embedd_dict[word.lower()])
+                pretrain_emb[index, :] = norm2one(embedd_dict[word.lower()])
             else:
-                pretrain_emb[index,:] = embedd_dict[word.lower()]
+                pretrain_emb[index, :] = embedd_dict[word.lower()]
             case_match += 1
         else:
-            pretrain_emb[index,:] = np.random.uniform(-scale, scale, [1, embedd_dim])
+            pretrain_emb[index, :] = np.random.uniform(-scale, scale, [1, embedd_dim])
             not_match += 1
     pretrained_size = len(embedd_dict)
-    print("Embedding:\n     pretrain word:%s, prefect match:%s, case_match:%s, oov:%s, oov%%:%s"%(pretrained_size, perfect_match, case_match, not_match, (not_match+0.)/alphabet_size))
+    print("Embedding:\n     pretrain word:%s, prefect match:%s, case_match:%s, oov:%s, oov%%:%s" % (
+    pretrained_size, perfect_match, case_match, not_match, (not_match + 0.) / alphabet_size))
     return pretrain_emb, embedd_dim
+
 
 def norm2one(vec):
     root_sum_square = np.sqrt(np.sum(np.square(vec)))
-    return vec/root_sum_square
+    return vec / root_sum_square
+
 
 def load_pretrain_emb(embedding_path):
     embedd_dim = -1
@@ -216,6 +222,7 @@ def load_pretrain_emb(embedding_path):
                 first_col = tokens[0]
             embedd_dict[first_col] = embedd
     return embedd_dict, embedd_dim
+
 
 if __name__ == '__main__':
     a = np.arange(9.0)
